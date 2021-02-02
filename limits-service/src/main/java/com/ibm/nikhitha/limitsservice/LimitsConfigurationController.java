0ff45bf6b4dc812162a191abcbd97b41dfe39725
nikhitha <nikijm114@gmail.com>
2021-02-02 18:53:54 +0530

    committing limit service to test config server changes

---
+++ limits-service/src/main/java/com/ibm/nikhitha/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,36 @@
+package com.ibm.nikhitha.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.nikhitha.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+
+@RestController
+@EnableHystrix
+public class LimitsConfigurationController {
+
+	@Autowired
+	Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(), 
+				configuration.getMinimum());
+		return limitConfiguration;
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
+
+}
